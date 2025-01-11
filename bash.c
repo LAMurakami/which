LAMurakami/which

commit a9a25e49c0c0db45468d7af0e407569518b4e2b5

Windows compat fixes, mostly gnuwin32
Allow compiling with Windows.  Mostly the patchset from https://gnuwin32.sourceforge.net/packages/which.htm updated and with the addition of proper windows home directory support.

(cherry picked from commit 547d65094c41065c549b605babcb05e2cae3b97b)

bash.c
@@ -21,8 +21,12 @@
 
 #include "sys.h"
 #include "posixstat.h"
+#if defined (HAVE_PWD_H)
 #include <pwd.h>
+#endif
+#ifndef _WIN32
 #include <unistd.h>
+#endif
 #include "bash.h"
 
 /* Use the type that was determined by configure. */
@@ -81,6 +85,7 @@ struct user_info current_user =
 int
 uidget ()
 {
+#ifndef _WIN32
   uid_t u;
 
   u = getuid ();
@@ -99,6 +104,9 @@ uidget ()
   /* See whether or not we are running setuid or setgid. */
   return (current_user.uid != current_user.euid) ||
            (current_user.gid != current_user.egid);
+#else
+	return 2;
+#endif
 }
 
 /* From bash-4.3 / general.c / line 1018 */
@@ -287,8 +295,11 @@ file_status (char const* name)
   if (current_user.euid == (uid_t)0)
     {
       r |= FS_READABLE;
+
+#ifdef S_IXGRP
       if (finfo.st_mode & S_IXUGO)
 	r |= FS_EXECABLE;
+#endif
       return r;
     }
 
@@ -304,19 +315,27 @@ file_status (char const* name)
   /* If we are in the owning group, the group permissions apply. */
   else if (group_member (finfo.st_gid))
     {
+#ifdef S_IXGRP
       if (finfo.st_mode & S_IXGRP)
 	r |= FS_EXECABLE;
+#endif
+#ifdef S_IRGRP
       if (finfo.st_mode & S_IRGRP)
 	r |= FS_READABLE;
+#endif
     }
 
   /* Else we check whether `others' have permission to execute the file */
   else
     {
+#ifdef S_IXOTH
       if (finfo.st_mode & S_IXOTH)
 	r |= FS_EXECABLE;
+#endif
+#ifdef S_IROTH
       if (finfo.st_mode & S_IROTH)
 	r |= FS_READABLE;
+#endif
     }
 
   return r;
@@ -330,7 +349,7 @@ file_status (char const* name)
 int
 absolute_program (char const* string)
 {
-  return ((char *)strchr (string, '/') != (char *)NULL);
+	return IS_ABSOLUTE(string);
 }
 
 /* From bash-4.3 / stringlib.c / line 124 */
@@ -373,10 +392,10 @@ extract_colon_unit (char const* string, int* p_index)
      `:'.  If I is 0, then the path has a leading colon.  Trailing colons
      are handled OK by the `else' part of the if statement; an empty
      string is returned in that case. */
-  if (i && string[i] == ':')
+  if (i && string[i] == PATH_SEPARATOR)
     i++;
 
-  for (start = i; string[i] && string[i] != ':'; i++)
+  for (start = i; string[i] && string[i] != PATH_SEPARATOR; i++)
     ;
 
   *p_index = i;
@@ -385,7 +404,7 @@ extract_colon_unit (char const* string, int* p_index)
     {
       if (string[i])
         (*p_index)++;
-      /* Return "" in the case of a trailing `:'. */
+      /* Return "" in the case of a trailing PATH_SEPARATOR (`:' resp. ';'). */
       value = (char *)xmalloc (1);
       value[0] = '\0';
     }
@@ -431,11 +450,11 @@ make_full_pathname (const char *path, const char *name, int name_len)
   path_len = strlen (path);
   full_path = (char *) xmalloc (2 + path_len + name_len);
   strcpy (full_path, path);
-  full_path[path_len] = '/';
+  full_path[path_len] = DIR_SEPARATOR;
   strcpy (full_path + path_len + 1, name);
   return (full_path);
 }
-
+#ifndef _WIN32
 /* From bash-4.3 / shell.c / line 1659 */
 void
 get_current_user_info ()
@@ -468,13 +487,14 @@ get_current_user_info ()
       endpwent ();
     }
 }
+#endif
 
 /* This is present for use by the tilde library. */
 char* sh_get_env_value (const char* v)
 {
   return getenv(v);
 }
-
+#ifndef _WIN32
 char* sh_get_home_dir(void)
 {
   if (current_user.home_dir == NULL)
@@ -482,3 +502,12 @@ char* sh_get_home_dir(void)
   return current_user.home_dir;
 }
 
+#else
+char* sh_get_home_dir(void)
+{
+  return get_home_dir();
+}
+int geteuid(void){
+	return 2;
+}
+#endif

bash.h
@@ -17,3 +17,4 @@ extern char *get_next_path_element(char const* path_list, int *path_index_pointe
 extern char *make_full_pathname(const char *path, const char *name, int name_len);
 extern int uidget();
 extern char* sh_get_home_dir(void);
+extern char* get_home_dir(void);

sys.h
@@ -34,6 +34,7 @@ char *strchr(), *strrchr();
 #else
 extern void *xmalloc(size_t);
 extern void *xrealloc(void *ptr, size_t size);
+#define XMALLOC_DEFINED 1
 #endif
 
 #ifndef NULL
@@ -51,3 +52,35 @@ extern void *xrealloc(void *ptr, size_t size);
 #   error "You either need getcwd(3) or getwd(3)"
 # endif
 #endif
+
+#ifdef _WIN32
+
+#define WIN32SYSTEM			1
+
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#define stat				_stati64
+#define getcwd				_getcwd
+#define strdup				_strdup
+#define isatty				_isatty
+#define strtok_r            strtok_s
+
+#define PATH_SEPARATOR		';'
+#define DIR_SEPARATOR		'\\'
+#define IS_SLASH(c)			((c) == '/' || (c) == DIR_SEPARATOR)
+#define SLASHES			"\\/"
+#define HAVE_DRIVE(n)		((n)[0] && (n)[1] == ':')
+#define IS_ABSOLUTE(n)        (IS_SLASH((n)[0]) || ((n)[0] && (n)[1] == ':'))
+#define DEFAULT_HOMEDIR		"C:\\"
+#else
+#define WIN32SYSTEM			0
+#define PATH_SEPARATOR		':'
+#define DIR_SEPARATOR		'/'
+#define IS_SLASH(c)			((c) == DIR_SEPARATOR)
+#define SLASHES			"/"
+#define HAVE_DRIVE(n)         (0)
+#define IS_ABSOLUTE(n)        ((n)[0] == DIR_SEPARATOR)
+#define DEFAULT_HOMEDIR		"/"
+#endif /* _WIN32  */
+#define IS_DIRSEP			IS_SLASH

which.c
@@ -22,6 +22,17 @@
 #include "getopt.h"
 #include "tilde/tilde.h"
 #include "bash.h"
+#include <ctype.h>
+#include <limits.h>
+
+#ifdef _WIN32
+# include <errno.h>
+# define DEFAULT_PATHEXT ".com;.exe;.bat;.cmd"
+# define LEN_DEFAULT_PATHEXT sizeof (DEFAULT_PATHEXT)
+# define PATHEXTSEPARATORS ";"
+# include <windows.h>
+# define PATH_MAX		MAX_PATH
+#endif /* WIN32 */
 
 static const char *progname;
 
@@ -63,7 +74,7 @@ static void print_fail(const char *name, const char *path_list)
   fprintf(stderr, "%s: no %s in (%s)\n", progname, name, path_list);
 }
 
-static char home[256];
+static char home[PATH_MAX];
 static size_t homelen = 0;
 
 static int absolute_path_given;
@@ -91,10 +102,11 @@ static char *find_command_in_path(const char *name, const char *path_list, int *
     if (abs_path)
       free(abs_path);
 
-    if (*name != '.' && *name != '/' && *name != '~')
+    if (*name != '.' && !IS_ABSOLUTE(name) && *name != '~')
     {
       abs_path = (char *)xmalloc(3 + name_len);
       strcpy(abs_path, "./");
+	 abs_path[1] = DIR_SEPARATOR;
       strcat(abs_path, name);
     }
     else
@@ -104,7 +116,7 @@ static char *find_command_in_path(const char *name, const char *path_list, int *
     }
 
     path_list = abs_path;
-    p = strrchr(abs_path, '/');
+    p = strrchr(abs_path, DIR_SEPARATOR);
     *p++ = 0;
     name = p;
   }
@@ -112,7 +124,6 @@ static char *find_command_in_path(const char *name, const char *path_list, int *
   while (path_list && path_list[*path_index])
   {
     char *path;
-
     if (absolute_path_given)
     {
       path = savestring(path_list);
@@ -137,32 +148,64 @@ static char *find_command_in_path(const char *name, const char *path_list, int *
       }
     }
 
-    if (skip_dot && *path != '/')
+    if (skip_dot && !IS_ABSOLUTE(path))
     {
       free(path);
       continue;
     }
 
     found_path_starts_with_dot = (*path == '.');
-
     full_path = make_full_pathname(path, name, name_len);
-    free(path);
 
     status = file_status(full_path);
-
     if ((status & FS_EXISTS) && (status & FS_EXECABLE))
     {
       found = full_path;
       break;
     }
 
+/* On MS-Windows also search for program name with executable extensions added */
+#ifdef _WIN32
+if (!found && !strrchr (name, '.')) {
+	LPTSTR PathExtStr, ext;
+	char *namex, *lasts;
+	int ext_len;
+
+	PathExtStr = strdup (getenv ("PATHEXT"));
+	if (!PathExtStr)
+		PathExtStr = strdup (DEFAULT_PATHEXT);
+	ext = strtok_r (PathExtStr, PATHEXTSEPARATORS, &lasts);
+	while (ext) {
+		free (full_path);
+		ext_len = strlen (ext);
+		namex = (char *) xmalloc (name_len + ext_len + 1);
+		if (!namex)
+			continue;
+		strcpy (namex, name);
+		strcat (namex, ext);
+	    full_path = make_full_pathname (path, namex, name_len + ext_len);
+		free (namex);
+		status = file_status (full_path);
+	     if ((status & FS_EXISTS) && (status & FS_EXECABLE)) {
+			found = full_path;
+			break;
+	    }
+		ext = strtok_r (NULL, PATHEXTSEPARATORS, &lasts);
+	}
+	if (PathExtStr)
+		free (PathExtStr);
+}
+#endif
+    free(path);
+	if (found)
+		break;
     free(full_path);
   }
 
   return (found);
 }
 
-static char cwd[256];
+static char cwd[PATH_MAX+1];
 static size_t cwdlen;
 
 static void get_current_working_directory(void)
@@ -177,7 +220,7 @@ static void get_current_working_directory(void)
       strcpy(cwd, pwd);
   }
 
-  if (*cwd != '/')
+  if (!IS_ABSOLUTE (cwd))
   {
     fprintf(stderr, "Can't get current working directory\n");
     exit(-1);
@@ -185,23 +228,23 @@ static void get_current_working_directory(void)
 
   cwdlen = strlen(cwd);
 
-  if (cwd[cwdlen - 1] != '/')
+  if (!IS_DIRSEP(cwd[cwdlen - 1]))
   {
-    cwd[cwdlen++] = '/';
+    cwd[cwdlen++] = DIR_SEPARATOR;
     cwd[cwdlen] = 0;
   }
 }
 
 static char *path_clean_up(const char *path)
 {
-  static char result[256];
+  static char result[PATH_MAX];
 
   const char *p1 = path;
   char *p2 = result;
 
   int saw_slash = 0, saw_slash_dot = 0, saw_slash_dot_dot = 0;
 
-  if (*p1 != '/')
+  if (!IS_ABSOLUTE (p1))
   {
     get_current_working_directory();
     strcpy(result, cwd);
@@ -215,11 +258,11 @@ static char *path_clean_up(const char *path)
      * Two leading slashes are allowed, having an OS implementation-defined meaning.
      * See http://www.opengroup.org/onlinepubs/009695399/basedefs/xbd_chap04.html#tag_04_11
      */
-    if (!saw_slash || *p1 != '/' || (p1 == path + 1 && p1[1] != '/'))
+    if (!saw_slash || !IS_DIRSEP (*p1) || (p1 == path + 1 && !IS_DIRSEP (p1[1])))
       *p2++ = *p1;
-    if (saw_slash_dot && (*p1 == '/'))
+    if (saw_slash_dot && (IS_DIRSEP (*p1)))
       p2 -= 2;
-    if (saw_slash_dot_dot && (*p1 == '/'))
+    if (saw_slash_dot_dot && (IS_DIRSEP (*p1)))
     {
       int cnt = 0;
       do
@@ -229,7 +272,7 @@ static char *path_clean_up(const char *path)
 	  strcpy(result, path);
 	  return result;
 	}
-	if (*p2 == '/')
+	if (!IS_DIRSEP (*p2))
 	  ++cnt;
       }
       while (cnt != 3);
@@ -237,7 +280,7 @@ static char *path_clean_up(const char *path)
     }
     saw_slash_dot_dot = saw_slash_dot && (*p1 == '.');
     saw_slash_dot = saw_slash && (*p1 == '.');
-    saw_slash = (*p1 == '/');
+    saw_slash = (IS_DIRSEP (*p1));
   }
   while (*p1++);
 
@@ -264,7 +307,7 @@ int func_search(int indent, const char *cmd, struct function_st *func_list, int 
   int i;
   for (i = 0; i < func_count; ++i)
   {
-    if (!strcmp(functions[i].name, cmd))
+    if (!stricmp(functions[i].name, cmd))
     {
       int j;
       if (indent)
@@ -301,13 +344,13 @@ int path_search(int indent, const char *cmd, const char *path_list)
       if (result)
       {
 	const char *full_path = path_clean_up(result);
-	int in_home = (show_tilde || skip_tilde) && !strncmp(full_path, home, homelen);
+	int in_home = (show_tilde || skip_tilde) && !strnicmp(full_path, home, homelen);
 	if (indent)
 	  fprintf(stdout, "\t");
-	if (!(skip_tilde && in_home) && show_dot && found_path_starts_with_dot && !strncmp(full_path, cwd, cwdlen))
+	if (!(skip_tilde && in_home) && show_dot && found_path_starts_with_dot && !strnicmp(full_path, cwd, cwdlen))
 	{
 	  full_path += cwdlen;
-	  fprintf(stdout, "./");
+	  fprintf(stdout, ".%c", DIR_SEPARATOR);
 	}
 	else if (in_home)
 	{
@@ -320,7 +363,7 @@ int path_search(int indent, const char *cmd, const char *path_list)
 	  if (show_tilde)
 	  {
 	    full_path += homelen;
-	    fprintf(stdout, "~/");
+	    fprintf(stdout, "~%c", DIR_SEPARATOR);
 	  }
 	}
 	fprintf(stdout, "%s\n", full_path);
@@ -343,7 +386,7 @@ void process_alias(const char *str, int argc, char *argv[], const char *path_lis
 
   while(*p == ' ' || *p == '\t')
     ++p;
-  if (!strncmp("alias", p, 5))
+  if (!strnicmp("alias", p, 5))
     p += 5;
   while(*p == ' ' || *p == '\t')
     ++p;
@@ -355,7 +398,7 @@ void process_alias(const char *str, int argc, char *argv[], const char *path_lis
     char q = 0;
     char *cmd;
 
-    if (!*argv || len != strlen(*argv) || strncmp(*argv, &p[-len], len))
+    if (!*argv || len != strlen(*argv) || strnicmp(*argv, &p[-len], len))
       continue;
 
     fputs(str, stdout);
@@ -385,9 +428,9 @@ void process_alias(const char *str, int argc, char *argv[], const char *path_lis
       cmd = (char *)xmalloc(len + 1);
       strncpy(cmd, &p[-len], len);
       cmd[len] = 0;
-      if (*argv && !strcmp(cmd, *argv))
+      if (*argv && !stricmp(cmd, *argv))
         *argv = NULL;
-      if (read_functions && !strchr(cmd, '/'))
+      if (read_functions && !strchr(cmd, DIR_SEPARATOR))
         found = func_search(1, cmd, functions, function_start_type);
       if (show_all || !found)
 	path_search(1, cmd, path_list);
@@ -428,7 +471,7 @@ static uid_t const superuser = 0;
 
 int main(int argc, char *argv[])
 {
-  const char *path_list = getenv("PATH");
+  char *path_list = getenv("PATH");
   int short_option, fail_count = 0;
   static int long_option;
   struct option longopts[] = {
@@ -448,6 +491,16 @@ int main(int argc, char *argv[])
   };
 
   progname = argv[0];
+#ifdef _WIN32
+	path_list = (char *) malloc (2 + strlen (path_list) + 1);
+	path_list[0] = '.';
+	path_list[1] = PATH_SEPARATOR;
+	path_list[2] = '\0';
+	strcat (path_list, getenv ("PATH"));
+	/* omit final ';' */
+	if (path_list [strlen (path_list) - 1] == PATH_SEPARATOR)
+		path_list [strlen (path_list) - 1] = '\0';
+#endif /* _WIN32 */
   while ((short_option = getopt_long(argc, argv, "aivV", longopts, NULL)) != -1)
   {
     switch (short_option)
@@ -515,9 +568,10 @@ int main(int argc, char *argv[])
     strncpy(home, h, sizeof(home));
     home[sizeof(home) - 1] = 0;
     homelen = strlen(home);
-    if (home[homelen - 1] != '/' && homelen < sizeof(home) - 1)
+    if (!IS_DIRSEP (home[homelen - 1]) && homelen < sizeof(home) - 1)
     {
-      strcat(home, "/");
+      home[homelen] = DIR_SEPARATOR;
+      home[homelen + 1] = '\0';
       ++homelen;
     }
   }
@@ -572,7 +626,7 @@ int main(int argc, char *argv[])
 	if (*p == ')' && p[-1] == '(' && p[-2] == ' ')
 	{
 	  looks_like_function_start = 1;
-	  function_start_has_declare = (strncmp("declare -", buf, 9) == 0);
+	  function_start_has_declare = (strnicmp("declare -", buf, 9) == 0);
 	}
 	// Add some zsh support here.
 	// zsh does output a pattern for `str' like
@@ -590,7 +644,7 @@ int main(int argc, char *argv[])
       if (processing_aliases && !looks_like_function_start)
       {
 	// bash version 2.0.5b can throw in lines like "declare -fx FUNCTION_NAME", eat them.
-	if (!strncmp("declare -", buf, 9))
+	if (!strnicmp("declare -", buf, 9))
 	  continue;
 	if (alias_count == max_alias_count)
 	{
@@ -636,7 +690,7 @@ int main(int argc, char *argv[])
 	{
 	  size_t blen = strlen(buf);
 	  function->lines[function->line_count++] = strcpy((char *)xmalloc(blen + 1), buf);
-	  if (!strcmp(buf, "}\n"))
+	  if (!stricmp(buf, "}\n"))
 	    break;
           if (function->line_count == max_line_count)
 	  {
@@ -661,16 +715,19 @@ int main(int argc, char *argv[])
     if (!*argv)
       continue;
 
-    if (read_functions && !strchr(*argv, '/'))
+    if (read_functions && !strchr(*argv, DIR_SEPARATOR))
       found_something = func_search(0, *argv, functions, function_start_type);
 
     if ((show_all || !found_something) && !path_search(0, *argv, path_list) && !found_something)
     {
-      print_fail(absolute_path_given ? strrchr(*argv, '/') + 1 : *argv, absolute_path_given ? abs_path : path_list);
+      print_fail(absolute_path_given ? strrchr(*argv, DIR_SEPARATOR) + 1 : *argv, absolute_path_given ? abs_path : path_list);
       ++fail_count;
     }
   }
-
+#ifdef _WIN32
+  if (path_list)
+	free (path_list);
+#endif
   return fail_count;
 }
 

Makefile.am
@@ -20,12 +20,10 @@ which_SOURCES = \
 	posixstat.h
 
 LDADD = $(top_builddir)/tilde/libtilde.a
-info_TEXINFOS = which.texi
-man_MANS = which.1
 
 # --------------- Maintainer's Section
 
-dist-local: which README EXAMPLES which.1
+dist-local: which README EXAMPLES
 
 distclean-local:
 	rm -rf .deps autom4te-*.cache which-*.tar.gz

maintMakefile.in
@@ -67,6 +67,8 @@ AUTHOR=Carlo Wood <carlo@gnu.org>
 TAG:=${shell echo "V@VERSION@" | sed -e 's/\./_/g'}
 PREVTAG:=${shell test -f $(srcdir)/.prevtag && cat $(srcdir)/.prevtag}
 CVSREADACCESS:=${shell if test -f $(srcdir)/CVS/Entries; then echo "yes"; else echo "no"; fi}
+info_TEXINFOS = which.texi
+man_MANS = which.1
 
 test-values:
 	@echo "WWWDIR        = \"$(WWWDIR)\""
@@ -116,7 +118,7 @@ ChangeLog:
 	  touch ChangeLog; \
 	fi
 
-release: dist cvslog html @PACKAGE@.lsm NEWS
+release: dist cvslog html  which.1 @PACKAGE@.lsm NEWS
 	install -m 644 index.html $(WWWDIR)
 	install -m 644 @PACKAGE@.lsm $(WWWDIR)
 	install -m 644 `ls $(srcdir)/cvslog-@VERSION@*.html` $(WWWDIR)

tilde/shell.c
@@ -43,7 +43,12 @@
 #  include <strings.h>
 #endif /* !HAVE_STRING_H */
 
+#ifndef _WIN32
 #include <pwd.h>
+#else
+#include <windows.h>
+#include <shlobj_core.h>
+#endif
 
 #if !defined (HAVE_GETPW_DECLS)
 extern struct passwd *getpwuid ();
@@ -59,6 +64,7 @@ get_env_value (varname)
 char *
 get_home_dir ()
 {
+#ifndef _WIN32
   char *home_dir;
   struct passwd *entry;
 
@@ -66,5 +72,13 @@ get_home_dir ()
   entry = getpwuid (getuid ());
   if (entry)
     home_dir = entry->pw_dir;
+#else
+	char *home_dir= xmalloc (sizeof (char) * (MAX_PATH));
+	if(! SUCCEEDED(SHGetFolderPath(NULL, CSIDL_PERSONAL, NULL, 0, home_dir))){
+		free(home_dir);
+		home_dir=NULL;
+	}
+
+#endif
   return (home_dir);
 }

tilde/tilde.c
@@ -48,6 +48,7 @@
 #endif
 
 #include "tilde.h"
+#include "../sys.h"
 
 #if defined (TEST) || defined (STATIC_MALLOC)
 static void *xmalloc (), *xrealloc ();
@@ -171,11 +172,7 @@ tilde_find_suffix (string)
 
   for (i = 0; i < string_len; i++)
     {
-#if defined (__MSDOS__)
-      if (string[i] == '/' || string[i] == '\\' /* || !string[i] */)
-#else
-      if (string[i] == '/' /* || !string[i] */)
-#endif
+      if (IS_DIRSEP(string[i]))
 	break;
 
       for (j = 0; suffixes && suffixes[j]; j++)
@@ -239,10 +236,10 @@ tilde_expand (string)
       free (tilde_word);
 
       len = strlen (expansion);
-#ifdef __CYGWIN__
+#if defined __CYGWIN__ || defined _WIN32
       /* Fix for Cygwin to prevent ~user/xxx from expanding to //xxx when
 	 $HOME for `user' is /.  On cygwin, // denotes a network drive. */
-      if (len > 1 || *expansion != '/' || *string != '/')
+      if (len > 1 || !IS_DIRSEP(*expansion) || IS_DIRSEP(*string))
 #endif
 	{
 	  if ((result_index + len + 1) > result_size)
@@ -271,11 +268,7 @@ isolate_tilde_prefix (fname, lenp)
   int i;
 
   ret = (char *)xmalloc (strlen (fname));
-#if defined (__MSDOS__)
-  for (i = 1; fname[i] && fname[i] != '/' && fname[i] != '\\'; i++)
-#else
-  for (i = 1; fname[i] && fname[i] != '/'; i++)
-#endif
+  for (i = 1; fname[i] && !IS_DIRSEP(fname[i]); i++)
     ret[i - 1] = fname[i];
   ret[i - 1] = '\0';
   if (lenp)
@@ -356,7 +349,7 @@ tilde_expand_word (filename)
   /* A leading `~/' or a bare `~' is *always* translated to the value of
      $HOME or the home directory of the current user, regardless of any
      preexpansion hook. */
-  if (filename[1] == '\0' || filename[1] == '/')
+  if (filename[1] == '\0' || IS_DIRSEP(filename[1]))
     {
       /* Prefix $HOME to the rest of the string. */
       expansion = sh_get_env_value ("HOME");

commit f32334745a8238d5af55a3172b4d4790f0c381f7

Fix the bug introduced by a9a25e49c0c0db45468d7af0e407569518b4e2b5
Without this fix you'd find the same executables
with "ls" as with "./ls":

$ which ./ls
/usr/bin/ls

while the correct reply is:
$ which ./ls
/usr/bin/which: no ls in (.)

Having any '/' at all in the search name means that
you DO specify a directory, either absolute, or relative
to the current directory (./...) or relative to your
home direction (~/...) and in all cases the PATH should
no longer be searched.

As the naming in the code (is_absolute) was just wrong, I can
understand this confusion; therefore I renamed the function
and even local variables as well to something that more clearly
states that this is about having a directory separator in the
name, NOT if it is an absolute path.

bash.c
@@ -351,9 +351,9 @@ file_status (char const* name)
    contains any slashes.  This is used to decide whether or not to look
    up through $PATH. */
 int
-absolute_program (char const* string)
+contains_separator (char const* string)
 {
-	return IS_ABSOLUTE(string);
+  return CONTAINS_SEPARATOR(string);
 }
 
 /* From bash-4.3 / stringlib.c / line 124 */

bash.h
@@ -12,7 +12,7 @@
 #define savestring(x) (char *)strcpy(xmalloc(1 + strlen (x)), (x))
 
 extern int file_status(const char *name);
-extern int absolute_program(const char *string);
+extern int contains_separator(const char *string);
 extern char *get_next_path_element(char const* path_list, int *path_index_pointer);
 extern char *make_full_pathname(const char *path, const char *name, int name_len);
 extern int uidget();

sys.h
@@ -61,35 +61,37 @@ extern void *xrealloc(void *ptr, size_t size);
 #include <sys/types.h>
 #include <ctype.h>
 
-#define stat				_stati64
-#define getcwd				_getcwd
-#define strdup				_strdup
-#define isatty				_isatty
-#define strtok_r            strtok_s
+#define stat			_stati64
+#define getcwd			_getcwd
+#define strdup			_strdup
+#define isatty			_isatty
+#define strtok_r                strtok_s
 
 #define PATH_SEPARATOR		';'
 #define DIR_SEPARATOR		'\\'
-#define IS_SLASH(c)			((c) == '/' || (c) == DIR_SEPARATOR)
+#define IS_SLASH(c)	        ((c) == '/' || (c) == DIR_SEPARATOR)
 #define SLASHES			"\\/"
 #define HAVE_DRIVE(n)		((n)[0] && (n)[1] == ':')
-#define IS_ABSOLUTE(n)        (IS_SLASH((n)[0]) || ((n)[0] && (n)[1] == ':'))
+#define IS_ABSOLUTE(n)          (IS_SLASH((n)[0]) || HAVE_DRIVE(n))
+#define CONTAINS_SEPARATOR(n)   (strchr(n, '/') != NULL || strchr(n, DIR_SEPARATOR) != NULL || HAVE_DRIVE(n))
 #define DEFAULT_HOMEDIR		"C:\\"
 #define STRSTR stristr
 #define STRCMP stricmp
 #define STRNCMP strnicmp
-#define CHAR_CMP(c1, c2) ( tolower(c1) == tolower(c2) )
+#define CHAR_CMP(c1, c2)        (tolower(c1) == tolower(c2))
 #else
-#define WIN32SYSTEM			0
+#define WIN32SYSTEM		0
 #define PATH_SEPARATOR		':'
 #define DIR_SEPARATOR		'/'
-#define IS_SLASH(c)			((c) == DIR_SEPARATOR)
+#define IS_SLASH(c)		((c) == DIR_SEPARATOR)
 #define SLASHES			"/"
-#define HAVE_DRIVE(n)         (0)
-#define IS_ABSOLUTE(n)        ((n)[0] == DIR_SEPARATOR)
+#define HAVE_DRIVE(n)           (0)
+#define IS_ABSOLUTE(n)          ((n)[0] == DIR_SEPARATOR)
+#define CONTAINS_SEPARATOR(n)   (strchr(n, DIR_SEPARATOR) != NULL)
 #define DEFAULT_HOMEDIR		"/"
 #define STRSTR strstr
 #define STRCMP strcmp
 #define STRNCMP strncmp
-#define CHAR_CMP(c1, c2) (c1 == c2)
+#define CHAR_CMP(c1, c2)        (c1 == c2)
 #endif /* _WIN32  */
-#define IS_DIRSEP			IS_SLASH
+#define IS_DIRSEP(c)		IS_SLASH(c)

which.c
@@ -77,7 +77,7 @@ static void print_fail(const char *name, const char *path_list)
 static char home[PATH_MAX];
 static size_t homelen = 0;
 
-static int absolute_path_given;
+static int path_containing_separator_given;
 static int found_path_starts_with_dot;
 static char *abs_path;
 
@@ -92,12 +92,12 @@ static char *find_command_in_path(const char *name, const char *path_list, int *
 
   name_len = strlen(name);
 
-  if (!absolute_program(name))
-    absolute_path_given = 0;
+  if (!contains_separator(name))
+    path_containing_separator_given = 0;
   else
   {
     char *p;
-    absolute_path_given = 1;
+    path_containing_separator_given = 1;
 
     if (abs_path)
       free(abs_path);
@@ -124,7 +124,7 @@ static char *find_command_in_path(const char *name, const char *path_list, int *
   while (path_list && path_list[*path_index])
   {
     char *path;
-    if (absolute_path_given)
+    if (path_containing_separator_given)
     {
       path = savestring(path_list);
       *path_index = strlen(path);
@@ -721,7 +721,7 @@ int main(int argc, char *argv[])
 
     if ((show_all || !found_something) && !path_search(0, *argv, path_list) && !found_something)
     {
-      print_fail(absolute_path_given ? strrchr(*argv, DIR_SEPARATOR) + 1 : *argv, absolute_path_given ? abs_path : path_list);
+      print_fail(path_containing_separator_given ? strrchr(*argv, DIR_SEPARATOR) + 1 : *argv, path_containing_separator_given ? abs_path : path_list);
       ++fail_count;
     }
   }